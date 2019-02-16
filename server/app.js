602fcb3f654117000999dca2a6451f59eb63740a
S-Kel <kel.s.stephen@gmail.com>
2019-02-16 12:39:21 +1000

    structured project folders

---
+++ server/app.js
@@ -0,0 +1,18 @@
+// Require in third-party modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
+// Create server from express
+const app = express();
+
+// Apply middlewares
+
+// Load routes
+
+// Catch 404 errors
+
+// Catch all other errors
+
+// Start server on port
